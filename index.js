2557da3840bda29303141cd0a40689c483d20458
allison craul <craul.aj@gmail.com>
2023-03-21 05:50:55 -0600

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(someValue){
+    const hq = 42
+    if(someValue>=42)  {
+       return (someValue - hq);
+    } else {
+        return (hq-someValue);
+    }
+    
+}
+
+distanceFromHqInBlocks(43);
+
+function distanceFromHqInFeet(someValue) {
+   return distanceFromHqInBlocks(someValue) * 264;
+}
+
+distanceFromHqInFeet(43);
+
+function distanceTravelledInFeet(start, destination) {
+    if (destination>start){
+        return ((destination-start)*264)
+} else{
+    return ((start-destination)*264)
+}
+}
+
+distanceTravelledInFeet(43,48);
+
+function calculatesFarePrice(start, destination) {
+    const distance = distanceTravelledInFeet(start, destination);
+
+    if(distance <= 400){
+        return 0;
+    } else if (distance> 400 && distance <= 2000) {
+        return .02 * (distance - 400)
+    } else if (distance > 2000 && distance < 2500) {
+        return 25;
+    } else{
+        return "cannot travel that far"
+    }
+}
+
+calculatesFarePrice(34,32);
